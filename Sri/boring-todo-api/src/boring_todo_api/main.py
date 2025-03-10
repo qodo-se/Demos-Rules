e3ccffc5a254179d1e6081a2b3d91e9802a4d509
Sri Rang <srirangan@gmail.com>
2025-03-10 11:26:21 +0100

    boring-todo-api — list /items

--- Sri/boring-todo-api/src/boring_todo_api/main.py
+++ Sri/boring-todo-api/src/boring_todo_api/main.py
@@ -1,12 +1,21 @@
-"""
-This is the main module of the boring-todo-api project.
-"""
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3001"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello World"}
+
+@app.get("/items")
+async def list_items():
+    return [
+        {"text": "hello world", "completed": False},
+        {"text": "foo bar", "completed": False},
+        {"text": "lorem ipsum", "completed": False},
+    ]
